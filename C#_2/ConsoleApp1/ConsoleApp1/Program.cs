fafc0453efcd8cdc8c4071bed223767abea52dc3
Orlos45 <januske.petr@seznam.cz>
2024-04-16 16:56:11 +0200

    Do dělat
    jop

---
+++ C#_2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class ComplexNumber
+    {
+        public double Real;
+        public double Imaginary;
+
+        public ComplexNumber(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        public void Add(ComplexNumber other)
+        {
+            Real += other.Real;
+            Imaginary += other.Imaginary;
+        }
+
+        public void Subtract(ComplexNumber other)
+        {
+            Real -= other.Real;
+            Imaginary -= other.Imaginary;
+        }
+
+        
+    }
+
+}
