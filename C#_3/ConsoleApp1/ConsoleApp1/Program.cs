6de94dcc989e9c923275bb86fb772da1a697cfb1
Orlos45 <januske.petr@seznam.cz>
2024-04-23 16:54:13 +0200

    List
    Pridat, Zpet, Vpred

---
+++ C#_3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            int index = 0;
+
+            while (true)
+            {
+                Console.Write("Zadejte příkaz:");
+                string input = Console.ReadLine();
+
+                if (input.StartsWith("Pridat."))
+                {
+                    string wordA = input.Substring(7).Trim();
+                    list.Add(wordA);
+                    index = list.Count - 1;
+                    Console.WriteLine($"Slovo '{wordA} bylo přidáno'");
+
+                }
+                else if (input == "Zpet")
+                {
+                    if (index >= 0)
+                    {
+                        Console.WriteLine($"Předchozí slovo: '{list[index]}'");
+                        index--;
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ždáné přechozí slovov není k dispozici");
+                    };
+
+                }
+                else if (input == "Vpred")
+                {
+                    if (index < list.Count - 1)
+                    {
+                        index++;
+                        Console.WriteLine($"Následují slovo: '{list[index]} ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ždáné přechozí slovov není k dispozici");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Neznámý příkaz. Použijte 'Pridat:', 'Zpet' nebo 'Vpred'.");
+                }
+            }
+            
+        }
+    }
+}
